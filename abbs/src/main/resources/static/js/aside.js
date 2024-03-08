c6b77b7baad3c037f6683f97b50ab83674eab199
SoonHyunAn <ash7106@gmail.com>
2024-03-08 15:50:25 +0900

    Spring

--- abbs/src/main/resources/static/js/aside.js
+++ abbs/src/main/resources/static/js/aside.js
@@ -12,16 +12,15 @@ $(document).ready(function () { // event 등록
 
 
 function changeStateMsg() {
-  let stateInputVal = $('#stateInput').val; // 사용자가 수정한 글 읽기
+  let stateInputVal = $('#stateInput').val(); // 사용자가 수정한 글 읽기
   $('#stateMsgInput').attr({ 'class': 'mt-2 d-none'}); // 입력창(stateMsgInput) 다시 안보이게
   $.ajax({  // Asychronous Javascript And XML - 화면에 일부분만 변경할 때 주로 사용
     type: 'GET', // 서버로 전송
-    url: 'abbs/aside/stateMsg', 
+    url: '/abbs/aside/stateMsg', 
     data: {stateMsg: stateInputVal},
     success: function(result){
-      console.log('stateMsg: ',stateInputVal, result); // 잘 갔는지 확인
+      console.log('state message: ',stateInputVal, result); // 잘 갔는지 확인
       $('#stateMsg').html(stateInputVal); // 변경한 값을 표시
     }
   });
-
 }
