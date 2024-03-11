aeaa7aec7c1a9962cff41fef398f5e958a49e5ae
SoonHyunAn <ash7106@gmail.com>
2024-03-11 14:29:16 +0900

    Spring

--- abbs/src/main/java/com/example/abbs/entity/Board.java
+++ abbs/src/main/java/com/example/abbs/entity/Board.java
@@ -13,8 +13,24 @@ public class Board {
 	private int replyCount;
 	private int likeCount;
 	private String files;
+	private String uname;
 
 	public Board() {
+	}
+
+	public Board(int bid, String title, String content, String uid, LocalDateTime modTime, int isDeleted, int viewCount,
+			int replyCount, int likeCount, String files, String uname) {
+		this.bid = bid;
+		this.title = title;
+		this.content = content;
+		this.uid = uid;
+		this.modTime = modTime;
+		this.isDeleted = isDeleted;
+		this.viewCount = viewCount;
+		this.replyCount = replyCount;
+		this.likeCount = likeCount;
+		this.files = files;
+		this.uname = uname;
 	}
 
 	public Board(int bid, String title, String content, String uid, LocalDateTime modTime, int isDeleted, int viewCount,
@@ -29,13 +45,6 @@ public class Board {
 		this.replyCount = replyCount;
 		this.likeCount = likeCount;
 		this.files = files;
-	};
-
-	@Override
-	public String toString() {
-		return "Board [bid=" + bid + ", title=" + title + ", content=" + content + ", uid=" + uid + ", modTime="
-				+ modTime + ", isDeleted=" + isDeleted + ", viewCount=" + viewCount + ", replyCount=" + replyCount
-				+ ", likeCount=" + likeCount + ", files=" + files + "]";
 	}
 
 	public int getBid() {
@@ -117,4 +126,20 @@ public class Board {
 	public void setFiles(String files) {
 		this.files = files;
 	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	@Override
+	public String toString() {
+		return "Board [bid=" + bid + ", title=" + title + ", content=" + content + ", uid=" + uid + ", modTime="
+				+ modTime + ", isDeleted=" + isDeleted + ", viewCount=" + viewCount + ", replyCount=" + replyCount
+				+ ", likeCount=" + likeCount + ", files=" + files + "]";
+	}
+
 }
