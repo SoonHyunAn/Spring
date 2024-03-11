d86a16ad2abe4ed4e008ba10bc7be5853ccc7587
SoonHyunAn <ash7106@gmail.com>
2024-03-11 12:23:41 +0900

    Spring

--- abbs/src/main/java/com/example/abbs/controller/AsideController.java
+++ abbs/src/main/java/com/example/abbs/controller/AsideController.java
@@ -1,7 +1,8 @@
 package com.example.abbs.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,32 +12,27 @@ import com.example.abbs.util.AsideUtil;
 
 import jakarta.servlet.http.HttpSession;
 
-
 @Controller
 @RequestMapping("/aside")
-@PropertySource("classpath:/static/data/myKeys.properties")
 public class AsideController {
 	@Autowired private AsideUtil asideUtil;
-	
-	
-	@GetMapping("/stateMsg")
+
 	@ResponseBody
+	@GetMapping("/stateMsg")
 	public String changeStateMsg(String stateMsg, HttpSession session) {
 		session.setAttribute("stateMeg", stateMsg);
 		return "return message"; // ResponseBody: 이번엔 데이터를 보내야 함.
 	}
-	
+
 	@ResponseBody
 	@GetMapping("/weather")
-	public String weather(HttpSession session) {
-		String location = (String) session.getAttribute("location")+"청";
-		String roadAddr;
-		try {
-			roadAddr = asideUtil.getRoadAddr(location);
-			return roadAddr;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+	public String weather(HttpSession session) throws Exception {
+		String location = (String) session.getAttribute("location") + "청";
+		String roadAddr = asideUtil.getRoadAddr(location);
+		Map<String, String> map = asideUtil.getGeocode(roadAddr);
+		String result = asideUtil.getWeather(map.get("lon"), map.get("lat"));
+		return result;
+//		return "lon= " + map.get("lon") + ", lat=" + map.get("lat"); // 좌표가 나오는 리턴값
+//		return roadAddr; // 도로명 주소가 나오는 리턴값
 	}
 }
--- abbs/src/main/resources/static/js/aside.js
+++ abbs/src/main/resources/static/js/aside.js
@@ -9,30 +9,30 @@ $(document).ready(function () { // event 등록
   });
   $('#stateMsgSubmit').click(changeStateMsg); // 이벤트 등록 - 오늘의 한줄 변경
 
-  $('getWeatherButton').click(getWeather); // 날씨
+  $('#getWeatherButton').click(getWeather); // 날씨
 });
 
 
 function changeStateMsg() {
   let stateInputVal = $('#stateInput').val(); // 사용자가 수정한 글 읽기
-  $('#stateMsgInput').attr({ 'class': 'mt-2 d-none'}); // 입력창(stateMsgInput) 다시 안보이게
+  $('#stateMsgInput').attr({ 'class': 'mt-2 d-none' }); // 입력창(stateMsgInput) 다시 안보이게
   $.ajax({  // Asychronous Javascript And XML - 화면에 일부분만 변경할 때 주로 사용
     type: 'GET', // 서버로 전송
-    url: '/abbs/aside/stateMsg', 
-    data: {stateMsg: stateInputVal},
-    success: function(result){
-      console.log('state message: ',stateInputVal, result); // 잘 갔는지 확인
+    url: '/abbs/aside/stateMsg',
+    data: { stateMsg: stateInputVal },
+    success: function (result) {
+      console.log('state message: ', stateInputVal, result); // 잘 갔는지 확인
       $('#stateMsg').html(stateInputVal); // 변경한 값을 표시
     }
   });
 }
 
-function getWeather(){
-  $.ajax({ 
+function getWeather() {
+  $.ajax({
     type: 'GET',
     url: '/abbs/aside/weather',
-    success: function(result){
+    success: function (result) {
       $('#weather').html(result);
-  }
-});
+    }
+  });
 }
